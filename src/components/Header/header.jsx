9918f173605ab72c25fd20e8a6bad2d2d7f6e54e
Joe Chang <131396136+josephachang02@users.noreply.github.com>
2024-01-23 22:05:21 -0800

    working on parts of footer

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -3,13 +3,12 @@ import './index.css'
 import Logo from "../../../src/Images/CSS/Logo.png";
 import Link from '@mui/material/Link';
 
-
 const Header = () => {
   const [shrink, setShrink] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 50) {
+      if (window.scrollY > 30) {
         setShrink(true);
       } else {
         setShrink(false);
@@ -22,85 +21,51 @@ const Header = () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-  
+
   return (
     <header className={shrink ? 'shrink' : ''}>
       <div className="logo-container">
         <a href="/">
-          <img id="Logo" src={Logo} alt="Logo" className="Logo" />
+          <img id="Logo" src={Logo} alt="Logo" className={`Logo ${shrink ? 'small-logo' : ''}`} />
         </a>
       </div>
       <nav>
-        {shrink ? (
-          <ul className="small-navbar" style = {{position:"fixed" }}>
-            {/* Small Navbar Links */}
-            <li>
-              <Link href="GIS" className="SmallNavTitle">
-                {'GIS'}
-              </Link>
-            </li>
-            <li>
-              <Link href="urban-planning" className="SmallNavTitle">
-                {'Urban Planning'}
-              </Link>
-            </li>
-            <li>
-              <Link href="software-engineering" className="SmallNavTitle">
-                {'Software Engineering'}
-              </Link>
-            </li>
-            <li>
-              <Link href="/Experience" className="SmallNavTitle">
-                {'Experience'}
-              </Link>
-            </li>
-            <li>
-              <Link href="/Projects" className="SmallNavTitle">
-                {'Projects'}
-              </Link>
-            </li>
-            <li>
-              <Link href="contact" className="SmallNavTitle">
-                {'Contact Me'}
-              </Link>
-            </li>
-          </ul>
-        ) : (
-          <ul className="navbar">
-            <li>
-              <Link href="GIS" className="NavTitle">
-                {'GIS'}
-              </Link>
-            </li>
-            <li>
-              <Link href="urban-planning" className="NavTitle">
-                {'Urban Planning'}
-              </Link>
-            </li>
-            <li>
-              <Link href="software-engineering" className="NavTitle">
-                {'Software Engineering'}
-              </Link>
-            </li>
-            <li>
-              <Link href="/Experience" className="NavTitle">
-                {'Experience'}
-              </Link>
-            </li>
-            <li>
-              <Link href="/Projects" className="NavTitle">
-                {'Projects'}
-              </Link>
-            </li>
-            <li>
-              <Link href="contact" className="NavTitle">
-                {'Contact Me'}
-              </Link>
-            </li>
-          </ul>
-        )}
+        <ul className={`navbar ${shrink ? 'small-navbar' : ''}`}>
+          {/* Small Navbar Links */}
+          <li>
+            <Link href="GIS" className="NavTitle">
+              {'GIS'}
+            </Link>
+          </li>
+          <li>
+            <Link href="urban-planning" className="NavTitle">
+              {'Urban Planning'}
+            </Link>
+          </li>
+          <li>
+            <Link href="software-engineering" className="NavTitle">
+              {'Software Engineering'}
+            </Link>
+          </li>
+          <li>
+            <Link href="/Experience" className="NavTitle">
+              {'Experience'}
+            </Link>
+          </li>
+          <li>
+            <Link href="/Projects" className="NavTitle">
+              {'Projects'}
+            </Link>
+          </li>
+          <li>
+            <Link href="contact" className="NavTitle">
+              {'Contact Me'}
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
-  )};
+  );
+};
 
 export default Header;
