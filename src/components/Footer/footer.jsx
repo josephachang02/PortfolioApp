3364ff5b7363a9c41aaabf5c1f45300ead094879
Joe Chang <131396136+josephachang02@users.noreply.github.com>
2024-01-29 19:41:53 -0800

    sample of footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import "./index.css"
 
 const footer = () => {
   return (
-    <div>footer</div>
-  )
+    <div>
+    <div className="logo">
+      <img src="your-logo.png" alt="Logo" />
+      <span>Your Logo Name</span>
+    </div>
+
+    <ul className="links">
+      <li><a href="#">Link 1</a></li>
+      <li><a href="#">Link 2</a></li>
+      <li><a href="#">Link 3</a></li>
+      <li><a href="#">Link 4</a></li>
+    </ul>
+
+    <div className="icons">
+      <a href="#"><img src="icon1.png" alt="Icon 1" /></a>
+      <a href="#"><img src="icon2.png" alt="Icon 2" /></a>
+      <a href="#"><img src="icon3.png" alt="Icon 3" /></a>
+      <a href="#"><img src="icon4.png" alt="Icon 4" /></a>
+    </div>
+  </div>
+);
 }
 
 export default footer
