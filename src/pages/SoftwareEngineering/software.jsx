3150cc3ba11e2a6d44158af217a61401d77b6398
Joe Chang <131396136+josephachang02@users.noreply.github.com>
2024-02-12 17:12:25 -0800

    changes to se page

--- src/pages/SoftwareEngineering/software.jsx
+++ src/pages/SoftwareEngineering/software.jsx
@@ -4,10 +4,15 @@ import { useState, useEffect } from 'react'
 import { IoLogoJavascript , IoLogoCss3} from "react-icons/io";
 import { FaHtml5 , FaReact , FaBootstrap , FaPython ,   } from "react-icons/fa"
 import { TbApi , TbSql  } from "react-icons/tb";
-import { SiMongodb , SiPandas , SiGdal  } from "react-icons/si";
+import { SiMongodb , SiPandas , SiGdal, SiMui  } from "react-icons/si";
 import { DiGithubBadge , DiPostgresql } from "react-icons/di";
 import Lottie from 'react-lottie'
 import SE from "../../../src/Images/CSS/se_working.json";
+import FinancePage from "../../../src/Images/CSS/FinancePage.png";
+import NBA_App from "../../../src/Images/CSS/NBA_APP.png";
+import WorkoutApp from "../../../src/Images/CSS/WorkoutApp.png"
+import Card from '@mui/material/Card'
+import CardContent  from '@mui/material/CardContent'
 
 
 const software = () => {
@@ -171,10 +176,129 @@ const software = () => {
           </div>
         </div>
 
+        <div id ="CardContainer">
+        <Card id = "SE-Card">
+          <CardContent class = "content">
+            <img
+              src= {FinancePage}
+              alt="example"
+              className="image"
+              style = {{maxWidth: "35%"}}
+            />
+        <div className="TextCard">
+        <h3>Personal Finance Web Page</h3>
+          <p>2023</p>
+          <p>Javascript functions to perform financial calculations based on the User's input</p>
+          <div className = "icon-image">
+            <IconButton
+              icon={<FaHtml5 />}
+              className="html-button"
+              href=""
+              onClick={() => {}}/>
+            <IconButton
+              icon={<IoLogoCss3 />}
+              className = "css3-button"
+              href=""
+              onClick={() => {}}/> 
+             <IconButton
+              icon={<IoLogoJavascript />}
+              className="javascript-button"
+              href=""
+              onClick={() => {}}/>
+            <IconButton
+              icon={<DiGithubBadge />}
+              className="github-button"
+              href=""
+              onClick={() => {}}/>
+          </div>
+          <button className ="buttonLink">< a href="https://github.com/josephachang02/FinanceApp" target="_blank" rel="noopener noreferrer">GitHub Link </a>
+          </button>
+          <button className ="buttonLink"><a href="https://peppy-custard-ae0f17.netlify.app" target="_blank" rel="noopener noreferrer">Link to Live Page</a>
+          </button>
+        </div>
+      </CardContent>
+    </Card>
 
+    <Card id = "SE-Card">
+          <CardContent class = "content">
+            <img
+              src= {NBA_App}
+              alt="example"
+              className="image"
+              style = {{maxWidth: "35%"}}
+            />
+        <div className="TextCard">
+        <h3>NBA APP</h3>
+          <p>2023</p>
+          <p>Utilizing React and useState components to provide a integrative site that utilizes information and data from Rapid API to pull information specific for team's and league wide statistics. On our home page, we have the full list of team names and their logo, I created a function that based on the click would change the useState to the selected Team, giving the information provided to other pages of the site. This allows useState to select information based on the criteria of the UseState (team selected). </p>
+          <div className = "icon-image">
+          <IconButton
+            icon={<FaReact />}
+            className="react-button"
+            href=""
+            onClick={() => {}}/>
+          <IconButton
+            icon={<TbApi />}
+            className="api-button"
+            href=""
+            onClick={() => {}}/>
+          <IconButton
+              icon={<FaBootstrap />}
+              className="bootstrap-button"
+              href=""
+              onClick={() => {}}/>
+          </div>
+          <button className ="buttonLink">< a href="https://github.com/josephachang02/FinanceApp" target="_blank" rel="noopener noreferrer">GitHub Link </a>
+          </button>
+          <button className ="buttonLink"><a href="https://peppy-custard-ae0f17.netlify.app" target="_blank" rel="noopener noreferrer">Link to Live Page</a>
+          </button>
+        </div>
+      </CardContent>
+    </Card>
 
+    <Card id = "SE-Card">
+          <CardContent class = "content">
+            <img
+              src= {WorkoutApp}
+              alt="example"
+              className="image"
+              style = {{maxWidth: "35%"}}
+            />
+        <div className="TextCard">
+        <h3>Workout APP</h3>
+          <p>2023</p>
+          <p>Utilizing React and useState components to provide a integrative site that utilizes information and data from Rapid API to pull information specific for team's and league wide statistics. On our home page, we have the full list of team names and their logo, I created a function that based on the click would change the useState to the selected Team, giving the information provided to other pages of the site. This allows useState to select information based on the criteria of the UseState (team selected). </p>
+          <div className = "icon-image">
+          <IconButton
+              icon={<FaReact />}
+              className="react-button"
+              href=""
+              onClick={() => {}}/>
+            <IconButton
+              icon={<TbApi />}
+              className = "api-button"
+              href=""
+              onClick={() => {}}/> 
+             <IconButton
+              icon={<SiMui />}
+              className="bootstrap-button"
+              href=""
+              onClick={() => {}}/>
+              <IconButton
+              icon={<SiMongodb />}
+              className="mongo-button"
+              href=""
+              onClick={() => {}}/>
+          </div>
+          <button className ="buttonLink">< a href="https://github.com/josephachang02/FinanceApp" target="_blank" rel="noopener noreferrer">GitHub Link </a>
+          </button>
+          <button className ="buttonLink"><a href="https://peppy-custard-ae0f17.netlify.app" target="_blank" rel="noopener noreferrer">Link to Live Page</a>
+          </button>
+        </div>
+      </CardContent>
+    </Card>
 
-
+</div>
 
       </div>
       );
